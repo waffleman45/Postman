88979d98eb1a36342d5e3e442346b61f99bc21d3
waffleman45 <brian.moore@blackboard.com>
2016-10-12 10:35:18 -0700

    Add files via upload

---
+++ ICPAutomationPrototype/ICPAutomationPrototype/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using PageActions;
+
+namespace ICPAutomationPrototype
+{
+    class AutomationFrontEnd
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Running UI tests on Chrome...");
+            PageObjectsAndActions action = new PageObjectsAndActions();
+            action.LogUserIn();
+            Console.ReadKey();
+        }
+    }
+}
